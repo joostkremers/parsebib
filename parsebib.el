0b22d5ddfa5f27110edcdd970fe3b827536adfad
Joost Kremers <joostkremers@fastmail.fm>
2017-03-07 11:42:50 +0100

    Check if define-error is bound, rather than checking version number

--- parsebib.el
+++ parsebib.el
@@ -47,27 +47,27 @@
 (defconst parsebib--entry-start "^[ \t]*@" "Regexp describing the start of an entry.")
 
 ;; Emacs 24.3 compatibility code.
-(if (version-list-< (version-to-list emacs-version) '(24 4))
-    ;; This definition is simply copied from the Emacs 24.4 sources
-    (defun define-error (name message &optional parent)
-      "Define NAME as a new error signal.
+(unless (fboundp 'define-error)
+  ;; This definition is simply copied from the Emacs 24.4 sources
+  (defun define-error (name message &optional parent)
+    "Define NAME as a new error signal.
 MESSAGE is a string that will be output to the echo area if such an error
 is signaled without being caught by a `condition-case'.
 PARENT is either a signal or a list of signals from which it inherits.
 Defaults to `error'."
-      (unless parent (setq parent 'error))
-      (let ((conditions
-             (if (consp parent)
-                 (apply #'nconc
-                        (mapcar (lambda (parent)
-                                  (cons parent
-                                        (or (get parent 'error-conditions)
-                                            (error "Unknown signal `%s'" parent))))
-                                parent))
-               (cons parent (get parent 'error-conditions)))))
-        (put name 'error-conditions
-             (delete-dups (copy-sequence (cons name conditions))))
-        (when message (put name 'error-message message)))))
+    (unless parent (setq parent 'error))
+    (let ((conditions
+           (if (consp parent)
+               (apply #'nconc
+                      (mapcar (lambda (parent)
+                                (cons parent
+                                      (or (get parent 'error-conditions)
+                                          (error "Unknown signal `%s'" parent))))
+                              parent))
+             (cons parent (get parent 'error-conditions)))))
+      (put name 'error-conditions
+           (delete-dups (copy-sequence (cons name conditions))))
+      (when message (put name 'error-message message)))))
 
 (define-error 'parsebib-entry-type-error "Illegal entry type" 'error)
 
