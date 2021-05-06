837c07be85bcf3a6c7f74a11c36e1f740dee993d
Joost Kremers <joostkremers@fastmail.fm>
2021-05-06 17:17:11 +0200

    Test more name fields than just author.

--- test/parsebib-test.el
+++ test/parsebib-test.el
@@ -30,25 +30,25 @@
   (should (string= (parsebib--json-stringify-name-field [((literal . "Michigan Institute of Technology"))])
                    "Michigan Institute of Technology")))
 
-;; Test `parsebib-stringify-json-field' with author field.
-(ert-deftest parsebib-test-stringify-json-field--author ()
+;; Test `parsebib-stringify-json-field' with name fields.
+(ert-deftest parsebib-test-stringify-json-field--name-fields ()
   (should (string= (parsebib-stringify-json-field '(author . [((family . "Anderson")
                                                                (given . "John R."))
                                                               ((family . "Bothell")
                                                                (given . "Daniel"))]))
                    "Anderson, John R. and Bothell, Daniel"))
-  (should (string= (parsebib-stringify-json-field '(author . [((family . "Koning")
+  (should (string= (parsebib-stringify-json-field '(editor . [((family . "Koning")
                                                                (given . "Willem")
                                                                (non-dropping-particle . "de"))
                                                               ((family . "Beethoven")
                                                                (dropping-particle . "van")
                                                                (given . "Ludwig"))]))
                    "de Koning, Willem and Beethoven, Ludwig van"))
-  (should (string= (parsebib-stringify-json-field '(author . [((family . "Gates")
-                                                               (given . "Bill")
-                                                               (suffix . "III"))]))
+  (should (string= (parsebib-stringify-json-field '(translator . [((family . "Gates")
+                                                                   (given . "Bill")
+                                                                   (suffix . "III"))]))
                    "Gates, Bill, III"))
-  (should (string= (parsebib-stringify-json-field '(author . [((literal . "Michigan Institute of Technology"))]))
+  (should (string= (parsebib-stringify-json-field '(recipient . [((literal . "Michigan Institute of Technology"))]))
                    "Michigan Institute of Technology")))
 
 ;;; parsebib-test.el ends here
