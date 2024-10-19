1daf42e92b103430151309c553ff04e72d724d3f
Joost Kremers <joostkremers@fastmail.com>
2024-10-19 12:02:18 +0200

    Fix indentation.

--- parsebib.el
+++ parsebib.el
@@ -279,7 +279,7 @@ See `parsebib-TeX-markup-replacement-alist' and the function
     ("h" . "\N{COMBINING HOOK ABOVE}")
     ("C" . "\N{COMBINING DOUBLE GRAVE ACCENT}")
     ("r" . "\N{COMBINING RING ABOVE}") )
-"Alist of <command>-<accent> pairs for LaTeX diacritics.
+  "Alist of <command>-<accent> pairs for LaTeX diacritics.
 <command> is the name of a TeX or LaTeX command (without
 backslash), <accent> is the Unicode combining character for the
 diacritic that <command> generates.  Both <command> and <accent>
@@ -337,9 +337,9 @@ See `parsebib-TeX-markup-replacement-alist' and the function
      ;; because doing it properly requires sexp parsing. It will fail
      ;; for cases like \command{\anothercommand{an arg}some text}.
      . ,(rx "\\" (group-n 1 (or (1+ letter) nonl))
-          (: (* blank) (opt (or (: (* (: "[" (* (not (any "]"))) "]"))
-                                 "{" (group-n 2 (0+ (not (any "}")))) (opt "}"))
-                                (group-n 3 letter))))))
+            (: (* blank) (opt (or (: (* (: "[" (* (not (any "]"))) "]"))
+                                     "{" (group-n 2 (0+ (not (any "}")))) (opt "}"))
+                                  (group-n 3 letter))))))
     (parsebib--TeX-replace-literal
      . ,(rx-to-string `(or ,@(mapcar #'car parsebib-TeX-literal-replacement-alist)
                            (1+ blank)))))
